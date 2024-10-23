239fe4011824087165dc4a04f0ae069b3bbaa307
GAURAVPANDEY004 <gaurav@code-aspire.com>
2024-10-23 13:09:57 +0530

    new project

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './redux/Store.js'
+import { Provider } from 'react-redux'
+
 // import SignIn from './pages/SignIn.jsx'
 // import Dashboard from './pages/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
